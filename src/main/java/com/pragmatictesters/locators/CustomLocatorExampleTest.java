cec25856b708978d26b5ebe1a40e678981390463
Janesh Kodikara <janesh@pragmatictesters.com>
2025-03-16 17:32:45 +0530

    Rename test with Test suffix.

--- src/main/java/com/pragmatictesters/locators/CustomLocatorExampleTest.java
+++ src/main/java/com/pragmatictesters/locators/CustomLocatorExampleTest.java
@@ -13,7 +13,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class CustomLocatorExample {
+public class CustomLocatorExampleTest {
 
     private WebDriver webDriver;
 
